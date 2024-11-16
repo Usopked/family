60fcf97684bf77e8e37d0ea55dd8b8e0a7008140
usopked <usopked16496@gmail.com>
2024-11-16 19:47:38 +0900

    calender

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // 기본 스타일 가져오기
 
 function App() {
+  const [value, setValue] = useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Calendar</h1>
+      <Calendar 
+        onChange={setValue} 
+        value={value} 
+      />
+      <p>선택한 날짜: {value.toDateString()}</p>
     </div>
   );
 }
